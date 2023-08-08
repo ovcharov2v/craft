56f052237aa61367882fd5a37c3508df3cb49295
Viktor Ovcharov <ovcharov.2v@gmail.com>
2023-08-08 20:45:57 +0400

    feat: add more btn to data-card

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import './vendor';
 
 import './components/_header';
 import './components/_section-instant';
+import './components/_section-data';
 import './components/_section-winners';
 import './components/_dropdown';
 import './components/_modal';
